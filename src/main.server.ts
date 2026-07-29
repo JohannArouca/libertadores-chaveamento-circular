32e8a14046ceb5e9cddbff5d88434057997efa49
Johann Arouca <aroucajohann@yahoo.com>
2026-07-28 21:44:34 -0300

    criacao do background

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,11 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
+import {
+  bootstrapApplication,
+  BootstrapContext,
+} from '@angular/platform-browser';
 import { config } from './app/app.config.server';
+import { App } from './app/app';
 
-const bootstrap = () => bootstrapApplication(App, config);
+const bootstrap = (context: BootstrapContext) =>
+  bootstrapApplication(App, config, context);
 
 export default bootstrap;
